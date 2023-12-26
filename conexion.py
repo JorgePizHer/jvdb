426a973431c0e07c966ae58cb8c248726d0d6229
JorgePizHer <jpizarroh166@alumnos.imf.com>
2023-12-26 17:49:56 +0100

    Creamos la clase

--- conexion.py
+++ conexion.py
@@ -1,17 +1,21 @@
 import subprocess
 
-operacion = "insert"
-basededatos = "miempresa"
-coleccion = "clientes"
-documento = "cliente3"
-contenido = "Este es un contenido de prueba"
+class Jvdb:
+    def __init__(self,basededatos):
+        self.basededatos = basededatos
+        
+    def insert(self,coleccion,documento,contenido):
 
+        self.operacion = "insert"
+        self.coleccion = coleccion
+        self.documento = documento
+        self.contenido = contenido
 
+        comando = '"D:\\OneDrive\\Escritorio\\Entornos de desarrollo\\GitHubRepos\\jvdb\\jvdb.exe" '+operacion+' '+basededatos+' '+coleccion+' '+documento+' "'+contenido+'"'
+        resultado = subprocess.run(comando,shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE,text=True)
 
-comando = '"D:\\OneDrive\\Escritorio\\Entornos de desarrollo\\GitHubRepos\\jvdb\\jvdb.exe" '+operacion+' '+basededatos+' '+coleccion+' '+documento+' "'+contenido+'"'
-resultado = subprocess.run(comando,shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE,text=True)
+        if resultado.returncode == 0:
+            return ("ok")
+        else:
+            return("ko")
 
-if resultado.returncode == 0:
-    print ("ok")
-else:
-    print("ko")
