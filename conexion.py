348fcfeb7ac0f2f307afb1c4b6a94f2a1a7a805c
JorgePizHer <jpizarroh166@alumnos.imf.com>
2023-12-26 17:41:03 +0100

    Parametrización

--- conexion.py
+++ conexion.py
@@ -1,6 +1,14 @@
 import subprocess
 
-comando = '"D:\\OneDrive\\Escritorio\\Entornos de desarrollo\\GitHubRepos\\jvdb\\jvdb.exe" insert miempresa clientes cliente2 "Juan Antonio"'
+operacion = "insert"
+basededatos = "miempresa"
+coleccion = "clientes"
+documento = "cliente3"
+contenido = "Este es un contenido de prueba"
+
+
+
+comando = '"D:\\OneDrive\\Escritorio\\Entornos de desarrollo\\GitHubRepos\\jvdb\\jvdb.exe" '+operacion+' '+basededatos+' '+coleccion+' '+documento+' "'+contenido+'"'
 resultado = subprocess.run(comando,shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE,text=True)
 
 if resultado.returncode == 0:
