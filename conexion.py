e8284b9b2c4fe7324adb19377bd73fa0fcabcaf3
JorgePizHer <jpizarroh166@alumnos.imf.com>
2023-12-27 17:17:10 +0100

    Ampliación del conector con métodos para actualizar, leer y eliminar

--- conexion.py
+++ conexion.py
@@ -1,4 +1,16 @@
 from jvdb_conector import Jvdb
 
-Conexion1 = Jvdb("miempresa")
-Conexion1.insert("clientes","cliente5","Más contenido de prueba")
+##Conexion1 = Jvdb("miempresa")
+##Conexion1.insert("clientes","cliente5","Más contenido de prueba")
+
+##Conexion2 = Jvdb("miempresa")
+##contenido = Conexion2.select("clientes","cliente5")
+
+##Conexion3 = Jvdb("miempresa")
+##Conexion3.insert("clientes","cliente4","Y ahora qué eedfdsfh")
+
+##Conexion4 = Jvdb("miempresa")
+##Conexion4.delete("clientes","cliente4")
+
+Conexion2 = Jvdb("miempresa")
+Conexion2.update("clientes","cliente2","Pepe","Pablo")
--- jvdb_conector.py
+++ jvdb_conector.py
@@ -1,22 +1,72 @@
 import subprocess
+import os
 
 class Jvdb:
     def __init__(self,basededatos):
         self.basededatos = basededatos
         
     def insert(self,coleccion,documento,contenido):
-
+        
         self.operacion = "insert"
         self.coleccion = coleccion
         self.documento = documento
         self.contenido = contenido
-
+        
         comando = '"D:\\OneDrive\\Escritorio\\Entornos de desarrollo\\GitHubRepos\\jvdb\\jvdb.exe" '+self.operacion+' '+self.basededatos+' '+self.coleccion+' '+self.documento+' "'+self.contenido+'"'
         resultado = subprocess.run(comando,shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE,text=True)
-
+        
         if resultado.returncode == 0:
             return ("ok")
         else:
             return("ko")
 
+    def update(self,coleccion,documento,contenidoviejo,contenidonuevo):
+        
+        self.operacion = "update"
+        self.coleccion = coleccion
+        self.documento = documento
+        self.contenidoviejo = contenidoviejo
+        self.contenidonuevo = contenidonuevo
+        
+        comando = '"D:\\OneDrive\\Escritorio\\Entornos de desarrollo\\GitHubRepos\\jvdb\\jvdb.exe" '+self.operacion+' '+self.basededatos+' '+self.coleccion+' '+self.documento+' "'+self.contenidoviejo+'" "'+self.contenidonuevo+'"'
+        resultado = subprocess.run(comando,shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE,text=True)
+        
+        if resultado.returncode == 0:
+            return ("ok")
+        else:
+            return("ko")
+    
+    def select(self,coleccion,documento):
 
+        self.operacion = "select"
+        self.coleccion = coleccion
+        self.documento = documento
+        
+        comando = '"D:\\OneDrive\\Escritorio\\Entornos de desarrollo\\GitHubRepos\\jvdb\\jvdb.exe" '+self.operacion+' '+self.basededatos+' '+self.coleccion+' '+self.documento+''
+        resultado = subprocess.run(comando,shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE,text=True)
+
+        
+        if resultado.returncode == 0:
+            return print(resultado.stdout.strip())
+        else:
+            return print("ko")
+
+    def delete(self,coleccion,documento):
+
+        self.operacion = "delete"
+        self.coleccion = coleccion
+        self.documento = documento
+        
+        comando = '"D:\\OneDrive\\Escritorio\\Entornos de desarrollo\\GitHubRepos\\jvdb\\jvdb.exe" '+self.operacion+' '+self.basededatos+' '+self.coleccion+' '+self.documento+''
+        resultado = subprocess.run(comando,shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE,text=True)
+
+        if os.path.exists(documento):
+              os.remove(documento)
+              print("Registro eliminado correctamente")
+        else:
+              print("The file does not exist")
+
+        if resultado.returncode == 0:
+            return print()
+        else:
+            return print("ko")
